fca01eed775c8f0ee15063e52ab16c5bbc5db6b4
Nathan Kulzer <nathan0kulzer@gmail.com>
2020-02-22 22:27:35 -0500

    Softcode port number

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Server:
         pid = os.fork()
         if pid == 0:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.bind(("", 47))
+            self.socket.bind(("", port))
             self.parent = sp[1]
         else:
             self.child = sp[0]
