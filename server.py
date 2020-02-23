f9174fa38a2b87ea69df57765c82cb4d75fceef3
Nathan Kulzer <nathan0kulzer@gmail.com>
2020-02-22 22:15:18 -0500

    Add the script files

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+import os
+
+
+class Server:
+    def __init__(self, port=47):
+        sp = socket.socketpair()
+        pid = os.fork()
+        if pid == 0:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind(("", 47))
+            self.parent = sp[1]
+        else:
+            self.child = sp[0]
+            self.pid = pid
+
+    def block(self):
+        pass
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.block()
